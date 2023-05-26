cb2bc43a11487d2f8d39bbf6d1d06764bfd9c9de
nkunabbp <112193936+nkunabbp@users.noreply.github.com>
2023-05-26 20:16:40 +0400

    Update Metod.cpp

--- classHomeWork2/Metod.cpp
+++ classHomeWork2/Metod.cpp
@@ -1,4 +1,4 @@
-#include"classfraction.h"
+#include"classFraction.h"
 
 void fraction::setnum(double Num) {
 	num = Num;
@@ -9,69 +9,73 @@ void fraction::setden(double Den) {
 }
 
 double fraction::multiplate(double number) {
-	double c{};
-	c = num / den;
-	c *= number;
-	return c;
+	double cNum{}, cDen{};
+	double returnNumber{};
+	cNum = num * number;
+	cDen = den * 1 ;
+	returnNumber = cNum / cDen;
+	return returnNumber;
 }
 double fraction::minus(double number) {
-	double c{};
-	c = num / den;
-	c -= number;
-	return c;
+	double cNum{}, cDen{};
+	double returnNumber{};
+	cNum = (num * 1) - (number * den);
+	cDen = den * 1;
+	returnNumber = cNum / cDen;
+	return returnNumber;
 }
 
-double fraction::plus(double number){
-	double c{};
-	c = num / den;
-	c += number;
-	return c;
+double fraction::plus(double number) {
+	double cNum{}, cDen{};
+	double returnNumber{};
+	cNum = (num * 1) + (number * den);
+	cDen = den * 1;
+	returnNumber = cNum / cDen;
+	return returnNumber;
 }
 
 double fraction::division(double number) {
-	double c{};
-	c = num / den;
-	c /= number;
-	return c;
+	double cNum{}, cDen{};
+	double returnNumber{};
+	cNum = num * 1;
+	cDen = den * number;
+	returnNumber = cNum / cDen;
+	return returnNumber;
 }
 
-
+//===================================================
 double fraction::multiplate(fraction fr) {
-	double c{};
+	double cNum{}, cDen{};
 	double number{};
-
-	number = fr.num / fr.den;
-	c = num / den;
-	c *= number;
-	return c;
+	cNum = num * fr.num;
+	cDen = den * fr.den;
+	number = cNum / cDen;
+	return number;
 }
 
 double fraction::minus(fraction fr) {
-	double c{};
+	double cNum{}, cDen{};
 	double number{};
-
-	number = fr.num / fr.den;
-	c = num / den;
-	c -= number;
-	return c;
+	cNum = (num * fr.den) - (fr.num * den);
+	cDen = den * fr.den;
+	number = cNum / cDen;
+	return number;
 }
 
 double fraction::plus(fraction fr) {
-	double c{};
+	double cNum{}, cDen{};
 	double number{};
-
-	number = fr.num / fr.den;
-	c = num / den;
-	c += number;
-	return c;
+	cNum = (num*fr.den) + (fr.num*den);
+	cDen = den * fr.den;
+	number = cNum / cDen;
+	return number;
 }
 
 double fraction::division(fraction fr) {
-	double c{};
+	double cNum{}, cDen{};
 	double number{};
-
-	number = fr.num / fr.den;
-	c = num / den;
-	c /= number;
-	return c;
+	cNum = num * fr.den;
+	cDen = den * fr.num;
+	number = cNum/cDen;
+	return number;
 }
